5af5c8ed0be8c216c18fe8319311d5870d49cf84
marcello de bonis <marcellodebonis.work@gmail.com>
2024-01-27 21:56:35 +0100

    Updates

--- Assets/Scripts/Mn_MicrofoneController.cs
+++ Assets/Scripts/Mn_MicrofoneController.cs
@@ -1,18 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
+using UGTK.Base.AudioSystem;
+using UGTK.Base.DamageSystem;
 using UnityEngine;
 
 public class Mn_MicrofoneController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private bool hasMicrofone = true;
+    private bool check = true;
+    [SerializeField] private float invicibilityTime = 1.0f;
+    [SerializeField] private Mn_HealthComponent healthComponent;
+    [SerializeField] private GameObject microfone;
+    [SerializeField] private List<GameObject> possibileSpawnPoints;
+    [SerializeField] private Mn_PlayerUi playerUi;
+    
+    [SerializeField] private List<int> lifePoints;
+    [SerializeField] private List<string> correspondingAudio;
+    
+    public void OnDamaged(float damage, float max)
     {
+        if (!check)
+        {   
+            check = true;
+            playerUi.DeactivateFirstActiveGameObject();
+            return;
+        }
         
+        if (!healthComponent.vulnerable)
+        {
+            return;
+        }
+        
+        if (hasMicrofone)
+        {
+            hasMicrofone = false;
+            SpawnMicrofoneInANewPosition();
+            healthComponent.vulnerable = true;
+            StartCoroutine(InvincibilityTimer());
+        }
+        else
+        {
+            healthComponent.TakeDamage(1);
+            float currentLife = healthComponent.GetLife();
+            int index = lifePoints.IndexOf((int)currentLife);
+            AudioEvents.AudioPlay.Invoke(correspondingAudio[index]);
+            check = false;
+        }    
+    }
+    
+    public IEnumerator InvincibilityTimer()
+    {
+        yield return new WaitForSeconds(invicibilityTime);
+        healthComponent.vulnerable = false;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SpawnMicrofoneInANewPosition()
     {
-        
+        int randomIndex = Random.Range(0, possibileSpawnPoints.Count);
+        microfone.SetActive(true);
+        microfone.transform.position = possibileSpawnPoints[randomIndex].transform.position;
     }
+    
+    public void GainMicrofone()
+    {
+        hasMicrofone = true;
+        microfone.SetActive(false);
+    }
+    
 }
