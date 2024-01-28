03c32d78c69dab25bc9736b7d4f16a6cea0168b3
marcello de bonis <marcellodebonis.work@gmail.com>
2024-01-28 16:48:19 +0100

    Missing : 1 GameManager: Do the damage to the enemy + Generate all possible worlds + tell to WorldManager to generate the 3 worlds. One can be selected. Every one of this world, has a personal Key that if it's pressed, the world is selected + select a random element in the all possible type + inside the WorldManager when somethings is wrotten, update the script "World" Attached into his object +  script of Multishooter, that shoot depending of the type

--- Assets/Scripts/Mn_GameManager.cs
+++ Assets/Scripts/Mn_GameManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UGTK.Base.DamageSystem;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Mn_GameManager : MonoBehaviour
 {
+    [SerializeField] private Mn_HealthComponent healthComponent;
+    
     public void LoseGame()
     {
         SceneManager.LoadScene("LoseScene");
--- Assets/Scripts/Mn_World.cs
+++ Assets/Scripts/Mn_World.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public struct WorldInfo
 {
-    public float TomatoSpawnDelay;
-    public GameObject TargetToShot;
+    public string world;
+    public SC_WorldType worldType;
 }
 
 public class Mn_World : MonoBehaviour
 {
+   [SerializeField] private TextMeshProUGUI worldWrottenr;
+   [SerializeField] private TextMeshProUGUI worldRest;
+
+   [SerializeField] private Image Type;
+   
    
 }
